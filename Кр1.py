04b7133fee9e7cf6129735d23ec39a3f3932a331
Vonzao <157824100+Vonzao@users.noreply.github.com>
2024-10-18 15:01:05 +0300

    Update Кр1.py

--- Кр1.py
+++ Кр1.py
@@ -30,17 +30,19 @@ class Lab:
 
         import math
 
-        for x in range(1, 10):
+        ans = []
+        for x in range(2, 20):
             x /= 20
-            y = (1+2*x**2) * math.e ** (x**2)
+            y = math.cosh(x)
             s = 0
             i = 0
             arg = float("inf")
             while abs(arg) >= 0.0001:
-                arg = ((2*i + 1) * x ** (2*i)) / math.factorial(i)
+                arg = x**(2*i) / math.factorial(2*i)
                 s += arg
                 i += 1
-            print(f"y: {y}; s: {s + 1}")
+            ans.append(f"y: {y}; s: {s}")
+        return '\n'.join(ans)
 
     @staticmethod
     @descriptions
@@ -50,12 +52,12 @@ class Lab:
         В группе учится n студентов. Каждый сдал 4 экзамена.
         Подсчитать число неуспевающих студентов и средний балл группы.
         """
-        values = [list(map(int, input(f'ВВедите оценки {i}-го ученика в формате "w x y z"').split()))
-                  for i in range(int(input("ВВедите количество учеников")))]
+        values = [list(map(int, input(f'ВВедите оценки {i+1}-го ученика в формате "w x y z": ').split()))
+                  for i in range(int(input("ВВедите количество учеников ")))]
         # Число неуспевающих - число учеников, у которых хотя бы одна двойка
         g = sum(any(value == 2 for value in values[i]) for i in range(len(values)))
         # Средний балл группы
-        s = sum(map(sum, values)) / len(values)
+        s = sum(map(sum, values)) / (len(values) * 4)
         return f"Число неуспевающих = {g}\nСредний балл группы = {s}"
 
     @staticmethod
@@ -66,3 +68,9 @@ class Lab:
         Из массива удалить повторяющиеся элементы.
         """
         return list(dict.fromkeys(array).keys())
+
+
+if __name__ == "__main__":
+    Lab.task7()
+    Lab.task11()
+    Lab.task13([1,3,1,2,2,5,4,1,6])
