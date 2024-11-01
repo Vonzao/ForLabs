a30956c7744b27bc0c788d72af2bfbc25f639951
Vonzao <157824100+Vonzao@users.noreply.github.com>
2024-11-01 11:31:12 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -4,8 +4,7 @@
                                     III Уровень
                                Сазонов Данила Сергеевич
 """
-
-
+import math
 from typing import Callable, Any
 
 
@@ -35,7 +34,7 @@ class Lab:
             return x * fac(x-1)
 
 
-        for x in range(1, 10):
+        for x in range(1, 11):
             x /= 10
             y = math.cos(x)
             s = 0
@@ -54,7 +53,7 @@ class Lab:
         import math
 
 
-        for x in range(1, 8):
+        for x in range(1, 9):
             x /= 10
             y = (x * math.sin(math.pi / 4)) / (1 - 2*x*math.cos(math.pi / 4) + x**2)
             s = 0
@@ -73,7 +72,7 @@ class Lab:
         import math
 
 
-        for x in range(1, 10):
+        for x in range(1, 11):
             x /= 10
             y = math.e ** math.cos(x) * math.cos(math.sin(x))
             s = 0
@@ -92,7 +91,7 @@ class Lab:
         import math
 
 
-        for x in range(1, 10):
+        for x in range(1, 11):
             x /= 10
             y = (1+2*x**2) * math.e ** (x**2)
             s = 0
@@ -104,9 +103,28 @@ class Lab:
                 i += 1
             print(f"y: {y}; s: {s}")
 
+    @staticmethod
+    @descriptions
+    def task5():
+
+        import math
+
+        for x in range(5, 26):
+            x = x / 25 * math.pi
+            y = (x**2 - math.pi**2 / 3) / 4
+            s = 0
+            i = 1
+            arg = float("inf")
+            while abs(arg) >= 0.0001:
+                arg = ((-1) ** i) * math.cos(i*x) / i**2
+                s += arg
+                i += 1
+            print(f"y: {y}; s: {s}")
+
 
 if __name__ == "__main__":
     Lab.task1()
     Lab.task2()
     Lab.task3()
     Lab.task4()
+    Lab.task5()
